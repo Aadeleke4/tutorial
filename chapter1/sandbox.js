3d4f8c5ac4c8b4392c433de7baf897ac13726d8e
adetumi <tumiadeleke@gmail.com>
2024-05-03 08:10:53 -0500

    initial commit

---
+++ chapter1/sandbox.js
@@ -0,0 +1,128 @@
+// let age = 25;
+// let year = 2022;
+
+// console.log(age + " is the current age." + " and  it's currently " + year);
+
+// age = 30;
+// console.log("Next year, " + age );
+
+// const points = 100;
+
+// console.log(`You have ${points} points.`);
+
+
+// // string
+// console.log('Hello, world');
+
+// let email = "john@example.com";
+// console.log(email);
+
+// //string concatenation
+// console.log('Hello, ' + 'world');
+
+
+// let fName = "daniel"
+// let sName = "Simpson"
+
+// console.log(fName + " and " +  sName); // SyntaxError: Missing )
+
+// //getting characters
+// console.log(email[0]);
+
+
+// console.log(fName.length)
+
+// //string methods
+// console.log(fName.toUpperCase());
+
+// console.log(email.indexOf("@"));
+
+
+
+// // math operators 
+
+// const pi = 3.14;
+// let radius = 10;
+
+// let result = pi * radius  * radius;
+// console.log(result);
+
+
+// //bodmas
+
+// let answer  = 2+3*5/8-9;
+// console.log(answer);
+
+
+// let likes = 10;
+
+// //likes = likes +2;
+// //likes++
+// likes +=20
+
+// console.log(likes);
+
+// //template string
+
+
+// const title = 'hello';
+// const name = 'tumi';
+// const takes = 100;
+
+// let _result = `${title} my name is ${name} I am taking ${takes}`;
+
+// console.log(_result);
+
+
+
+// // arrays
+
+// let ninjas = ['daniel','chunli','tumi'];
+
+// console.log(ninjas[2]);
+
+
+// // boolean
+// console.log(true && false) //false
+// console.log(true || false) // true
+// console.log(!true)          // false
+
+// console.log(true, false, "true",  "false")
+
+// let mail = 'tumiadeleke@gmail.com'
+
+// let results = mail.includes('@');
+// console.log(results);
+
+// if (true){
+//     console.log('it is true');
+// }
+// else {
+//     console.log('it is false')
+// }
+
+// let age = 30;
+
+// console.log(age ==30);
+// console.log(age != 30);
+// console.log(age < 30);
+// console.log(age > 30);
+// console.log(age <= 30);     
+// console.log(age >= 30);
+
+// let name1 = 'Daniel';
+
+// console.log(name1 === 'Daniel');   // strict equality checks both value and type of variables   
+// console.log(name1 === 'daniel');
+// console.log(name1 !== 'Daniel');     // not equal to
+// console.log(name1 >  'Alice');       // string comparison in alphabet
+
+
+
+let score = '100';
+
+// score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);                         // returns number
+
